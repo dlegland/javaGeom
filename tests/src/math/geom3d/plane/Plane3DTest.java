96ff8810d08cefef45135f881a0633a3291b2816
dlegland <dlegland@09ec13b7-e712-4093-b2cc-05b6e6a3d9fa>
2008-05-22 21:01:34 +0000

    geom3d: add LineSegment3D, update Plane3D
    git-svn-id: svn://svn.code.sf.net/p/geom-java/code/trunk@136 09ec13b7-e712-4093-b2cc-05b6e6a3d9fa

--- tests/src/math/geom3d/plane/Plane3DTest.java
+++ tests/src/math/geom3d/plane/Plane3DTest.java
@@ -36,7 +36,64 @@ public class Plane3DTest extends TestCase {
 		
 		assertTrue(p1.equals(pInt));
 	}
+	
+	public void testProjectPoint() {
+		Plane3D plane 	= Plane3D.createXYPlane();
+		Point3D p0 	= new Point3D(3, 4, 0);
+		Point3D p1 	= new Point3D(3, 4, 10);
+		Point3D p2 	= new Point3D(3, 4, -10);
 
+		assertTrue(p0.equals(plane.projectPoint(p1)));
+		assertTrue(p0.equals(plane.projectPoint(p2)));
+
+		plane 	= Plane3D.createXZPlane();
+		p0 	= new Point3D(3, 0, 4);
+		p1 	= new Point3D(3, 10, 4);
+		p2 	= new Point3D(3, -10, 4);
+
+		assertTrue(p0.equals(plane.projectPoint(p1)));
+		assertTrue(p0.equals(plane.projectPoint(p2)));
+
+		plane 	= Plane3D.createYZPlane();
+		p0 	= new Point3D(0, 3, 4);
+		p1 	= new Point3D(10, 3, 4);
+		p2 	= new Point3D(-10, 3, 4);
+
+		assertTrue(p0.equals(plane.projectPoint(p1)));
+		assertTrue(p0.equals(plane.projectPoint(p2)));
+	}
+
+	public void testProjectVector() {
+		Vector3D vx 	= new Vector3D(1, 0, 0);
+		Vector3D vy 	= new Vector3D(0, 1, 0);
+		Vector3D vz 	= new Vector3D(0, 0, 1);
+
+		Plane3D plane 	= new Plane3D(new Point3D(10, 5, 8), vx, vy);
+		Vector3D v0 	= new Vector3D(3, 4, 0);
+		Vector3D v1 	= new Vector3D(3, 4, 10);
+		Vector3D v2 	= new Vector3D(3, 4, -10);
+
+		assertTrue(v0.equals(plane.projectVector(v1)));
+		assertTrue(v0.equals(plane.projectVector(v2)));
+
+		plane 	= new Plane3D(new Point3D(10, 5, 8), vx, vz);
+		v0 	= new Vector3D(3, 0, 4);
+		v1 	= new Vector3D(3, 10, 4);
+		v2 	= new Vector3D(3, -10, 4);
+
+		assertTrue(v0.equals(plane.projectVector(v1)));
+		assertTrue(v0.equals(plane.projectVector(v2)));
+
+		plane 	= new Plane3D(new Point3D(10, 5, 8), vy, vz);
+		v0 	= new Vector3D(0, 3, 4);
+		v1 	= new Vector3D(10, 3, 4);
+		v2 	= new Vector3D(-10, 3, 4);
+
+		assertTrue(v0.equals(plane.projectVector(v1)));
+		assertTrue(v0.equals(plane.projectVector(v2)));
+	}
+
+	
 	/**
 	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
 	 */
