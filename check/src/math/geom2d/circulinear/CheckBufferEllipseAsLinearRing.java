b63ab0f47052decc71d649ba8bd18d4868a7dc3e
dlegland <dlegland@09ec13b7-e712-4093-b2cc-05b6e6a3d9fa>
2009-10-18 10:40:42 +0000

    fix buffer of curves with parallel tangent vectors
    git-svn-id: svn://svn.code.sf.net/p/geom-java/code/trunk@231 09ec13b7-e712-4093-b2cc-05b6e6a3d9fa

---
+++ check/src/math/geom2d/circulinear/CheckBufferEllipseAsLinearRing.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package math.geom2d.circulinear;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import math.geom2d.conic.Ellipse2D;
+import math.geom2d.domain.Domain2D;
+import math.geom2d.polygon.LinearRing2D;
+
+/**
+ * creates an ellipse, convert to linear ring, and computes buffer.
+ * @author dlegland
+ *
+ */
+public class CheckBufferEllipseAsLinearRing extends JPanel{
+	private static final long serialVersionUID = 1L;
+
+	Ellipse2D ellipse;
+	LinearRing2D ring;
+	
+	double dist = 20;
+	
+	public CheckBufferEllipseAsLinearRing(){
+		
+		ellipse = new Ellipse2D(300, 200, 200, 100, Math.PI/6);
+		ring = (LinearRing2D) ellipse.getAsPolyline(4);
+	}
+	
+	public void paintComponent(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		
+		// compute buffer
+		Domain2D buffer = CirculinearCurve2DUtils.computeBuffer(ring, dist);
+		
+		// fill the buffer
+		g2.setColor(Color.CYAN);
+		buffer.fill(g2);
+
+		g2.setColor(Color.BLACK);
+		ellipse.draw(g2);
+		
+		
+		g2.setColor(Color.BLUE);
+		ring.draw(g2);
+	}
+	
+	public final static void main(String[] args){
+		System.out.println("check buffer of an approximated ellipse");
+		
+		JPanel panel = new CheckBufferEllipseAsLinearRing();
+		panel.setPreferredSize(new Dimension(600, 600));
+		JFrame frame = new JFrame("Approximated ellipse buffer");
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setVisible(true);		
+	}
+}
---
+++ check/src/math/geom2d/circulinear/CheckGetBufferPolylineColinear.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package math.geom2d.circulinear;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import math.geom2d.Point2D;
+import math.geom2d.polygon.Polyline2D;
+
+/**
+ * Compute buffer of a polyline with colinear adjacent edges
+ * @author dlegland
+ *
+ */
+public class CheckGetBufferPolylineColinear extends JPanel{
+	private static final long serialVersionUID = 1L;
+
+	CirculinearCurve2D curve;
+	CirculinearDomain2D domain;
+	
+	public CheckGetBufferPolylineColinear(){
+		
+		curve = new Polyline2D(new Point2D[]{
+				new Point2D(100, 100), 
+				new Point2D(150, 100), 
+				new Point2D(200, 100), 
+				new Point2D(200, 150), 
+				new Point2D(200, 200),
+				new Point2D(150, 200),
+				new Point2D(100, 200),
+				new Point2D(150, 250),
+				new Point2D(200, 300),
+				new Point2D(250, 250),
+				new Point2D(300, 300) });
+		
+		domain = curve.getBuffer(30);
+	}
+	
+	public void paintComponent(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.setColor(Color.CYAN);
+		domain.fill(g2);
+		g2.setColor(Color.BLUE);
+		domain.draw(g2);
+		
+		g2.setColor(Color.BLACK);
+		curve.draw(g2);
+	}
+	
+	public final static void main(String[] args){
+		JPanel panel = new CheckGetBufferPolylineColinear();
+		panel.setPreferredSize(new Dimension(500, 400));
+		JFrame frame = new JFrame("Compute buffer of a polyline");
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setVisible(true);		
+	}
+}
