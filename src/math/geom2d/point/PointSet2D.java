aa3deb8d830963d8bf203d18a21e35e5215ee948
dlegland <david.legland@nantes.inra.fr>
2009-10-27 17:41:59 +0000

    minor clean up in Point sets

--- src/math/geom2d/point/PointSet2D.java
+++ src/math/geom2d/point/PointSet2D.java
@@ -16,13 +16,15 @@ import math.geom2d.Point2D;
 
 
 /**
+ * A set of points. All points within the set are instances of Point2D.
+ * The most direct implementation of PointSet2D is PointArray2D.
  * @author dlegland
  *
  */
 public interface PointSet2D extends PointShape2D, Iterable<Point2D> {
 
     /**
-     * Adds a new point to the set of point. If point is not an instance of
+     * Adds a new point to the point set. If point is not an instance of
      * Point2D, a Point2D with same location is added instead of point.
      * 
      * @param point the initial point in the set
@@ -34,7 +36,7 @@ public interface PointSet2D extends PointShape2D, Iterable<Point2D> {
      * 
      * @param points an array of points
      */
-    public void addPoints(Collection<Point2D> points);
+    public void addPoints(Collection<? extends Point2D> points);
 
     /**
      * Returns an iterator on the internal point collection.
@@ -50,7 +52,14 @@ public interface PointSet2D extends PointShape2D, Iterable<Point2D> {
      */
     public int getPointNumber();
     
+    /**
+     * Transforms the point set by returning a new point set containing each 
+     * transformed point.
+     */
     public abstract PointSet2D transform(AffineTransform2D trans);
     
+    /**
+     * Returns a new point set containing only points located within the box.
+     */
     public abstract PointSet2D clip(Box2D box);
 }
