a9a90080d881d55c0af85f17fdd0a2edf840ffc2
dlegland <david.legland@nantes.inra.fr>
2010-08-18 15:40:44 +0000

    added the GPCJ library, that handle union and intersection of polygons

--- makejar.bat
+++ makejar.bat
@@ -1,4 +1,4 @@
-@REM Build the file Euclide.jar
+@REM Build the file javaGeom.jar
 @REM
 @REM Requires the file 'jar.exe' to be in the path
 @REM For adding it with windows (Windows Vista)
@@ -10,14 +10,32 @@
 @REM 	(Adapt version number according to your system)
 @REM
 
+@REM modified for incorporating external library gpcj (polygon clipping)
+
 @echo off
 
+@REM remove old copy of jar folder if it exists
+FOR /F "tokens=*" %%i IN ('dir /a:d /b jar') DO rmdir /S /Q %%i
+
+@REM make a copy of the binary directory into the jar directory
+robocopy bin jar *.* /s /njs /njh /ndl
+
 @REM remove '.svn' directories, created by Eclipse from subversion repository
-FOR /F "tokens=*" %%i IN ('dir /a:d /s /b bin\*.svn*') DO rmdir /S /Q %%i
+FOR /F "tokens=*" %%i IN ('dir /a:d /s /b jar\*.svn*') DO rmdir /S /Q %%i
+
+@REM change directory and uncompress external library
+cd jar
+jar xf ../lib/gpcj.jar
+rmdir /S /Q META-INF
+cd ..
 
 @REM remove old archive if it exists
-FOR /F "tokens=*" %%i IN ('dir /b javaGeom.jar') DO del /f /q %%i
+@REM FOR /F "tokens=*" %%i IN ('dir /b javaGeom.jar') DO del /f /q %%i
+if exist javaGeom.jar del /f /q javaGeom.jar
 
 @REM build the new archive
-jar cvf javaGeom.jar -C bin\ .
+jar cf javaGeom.jar -C jar\ .
 
+@REM remove temporary files created during process
+:: FOR /F "tokens=*" %%i IN ('dir /a:d /b jar') DO rmdir /S /Q %%i
+if exist jar rmdir /S /Q jar
