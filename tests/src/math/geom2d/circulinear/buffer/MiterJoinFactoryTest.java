52756030b923027421e3e0da4f9c0ab38d41b3d4
dlegland <david.legland@nantes.inra.fr>
2011-09-17 09:20:24 +0000

    add (simple) tests for join and cap factories

--- tests/src/math/geom2d/circulinear/buffer/MiterJoinFactoryTest.java
+++ tests/src/math/geom2d/circulinear/buffer/MiterJoinFactoryTest.java
@@ -6,7 +6,10 @@ package math.geom2d.circulinear.buffer;
 import math.geom2d.Point2D;
 import math.geom2d.Shape2D;
 import math.geom2d.curve.Curve2D;
+import math.geom2d.domain.Boundary2D;
+import math.geom2d.domain.Domain2D;
 import math.geom2d.line.LineSegment2D;
+import math.geom2d.polygon.LinearRing2D;
 import math.geom2d.polygon.Polyline2D;
 import junit.framework.TestCase;
 
@@ -16,6 +19,47 @@ import junit.framework.TestCase;
  */
 public class MiterJoinFactoryTest extends TestCase {
 
+	public void testGetBufferPolyline () {
+		Polyline2D curve = new Polyline2D(new Point2D[]{
+				new Point2D(50, 50), 
+				new Point2D(50, 100), 
+				new Point2D(100, 100), 
+				new Point2D(100, 50), 
+				new Point2D(150, 100), 
+				new Point2D(150, 50), 
+				});
+
+		BufferCalculator bc1 = new BufferCalculator(
+				new MiterJoinFactory(), new RoundCapFactory());
+		Domain2D domain = bc1.computeBuffer(curve, 20);
+
+		assertTrue(domain.isBounded());
+		assertFalse(domain.isEmpty());
+		
+		Boundary2D boundary = domain.getBoundary();
+		assertEquals(1, boundary.getBoundaryCurves().size());
+	}
+
+	public void testGetBufferLinearRing () {
+		LinearRing2D curve = new LinearRing2D(new Point2D[]{
+				new Point2D(100, 100), 
+				new Point2D(200, 100), 
+				new Point2D(200, 200), 
+				new Point2D(150, 150), 
+				new Point2D(100, 200), 
+				});
+
+		BufferCalculator bc1 = new BufferCalculator(
+				new MiterJoinFactory(), new RoundCapFactory());
+		Domain2D domain = bc1.computeBuffer(curve, 10);
+
+		assertTrue(domain.isBounded());
+		assertFalse(domain.isEmpty());
+		
+		Boundary2D boundary = domain.getBoundary();
+		assertEquals(2, boundary.getBoundaryCurves().size());
+	}
+	
 	/**
 	 * Test method for {@link math.geom2d.circulinear.buffer.MiterJoinFactory#createJoin(math.geom2d.circulinear.CirculinearElement2D, math.geom2d.circulinear.CirculinearElement2D, double)}.
 	 */
